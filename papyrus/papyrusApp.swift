7fb03f7f55a73afa922adfb1f9b53ab065dc3d16
Pavel Kroupa <63880977+Tabonx@users.noreply.github.com>
2025-06-19 21:23:07 +0200

    Initial Commit

---
+++ papyrus/papyrusApp.swift
@@ -0,0 +1,17 @@
+//
+//  papyrusApp.swift
+//  papyrus
+//
+//  Created by Pavel Kroupa on 19.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct papyrusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
